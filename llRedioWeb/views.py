48b3bcbb90996ccbcef5f36a040fe896cbc7f289
RicterZ <RicterZheng@gmail.com>
2013-12-24 13:03:34 +0800

    能播放音乐了QAQ

--- llRedioWeb/views.py
+++ llRedioWeb/views.py
@@ -12,21 +12,13 @@ def TypeHandler(request):
     return HttpResponse('Hello World! I am TypeHandler!')
 	
 def MusicHandler(request):
-    getDownkeyUrl = 'http://downkey.music.qq.com/fcgi-bin/fcg_create_downkey2.fcg'
-    getDataDict = {
-        'musicid': request.GET.get('musicid', ''),
-        'quality': 0,
-    }
-    params = urllib.urlencode(getDataDict)
-    req = urllib2.Request('%s?%s' % (getDownkeyUrl, params), None)
-    returnXML = urllib2.urlopen(req).read()
-    urlGetter = re.search('<url>(.*)</url>', returnXML)
-    resultGetter = re.search('<result>(.*)</result>', returnXML)
-    if resultGetter.group(1) == '0':
-        return HttpResponse(json.dumps({
-            "code": 0,
-            "url": urlGetter.group(1)
-        }))
+    url = "http://ws.stream.qqmusic.qq.com/C200"
+    vkey = "A430C496F97CC9BD1F901A5F17087E09A0BF9A77370AC40B882B6B82214E3A6D"
+    guid = "950085250"
+    return HttpResponse(json.dumps({
+        "code": 0,
+        "url": "%s%s.m4a?vkey=%s&guid=%s" % (url, request.GET.get('musicMid', ''), vkey, guid)
+    }))
 	
 def SearchHandler(request):
     searchUrl = 'http://soso.music.qq.com/fcgi-bin/music_search_new_platform'
@@ -39,7 +31,7 @@ def SearchHandler(request):
         'p': request.GET.get('p', '1').encode('utf-8'),
     }
     params = urllib.urlencode(getDataDict)
-    print params
+    #print params
     req = urllib2.Request('%s?%s' % (searchUrl, params), None)
     returnRawJson = urllib2.urlopen(req).read()
     cleanJson = re.match('MusicJsonCallback(.*)', returnRawJson)
@@ -58,6 +50,7 @@ def SearchHandler(request):
         'list': [{
             'musicTitle': musicItem['fsong'],
             'musicID': re.split('[@|]{1,2}', musicItem['f'])[0],
+            'musicMid':  re.split('[@|]{1,2}', musicItem['f'])[-3],
             'singer': musicItem['fsinger'],
             'singerImg': singImgUrl % (musicItem['singerMID'][-2], \
                          musicItem['singerMID'][-1], musicItem['singerMID']),
